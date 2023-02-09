081272941a64f072e4bc41ccff70d808685d3d0f
= <=>
2023-02-09 17:24:14 +0100

    first try

---
+++ variable.tf
@@ -0,0 +1,9 @@
+# NECESSARY
+#-----------
+variable "region"     { default = "us-east-1" }
+variable "access_key" { default = "ACCES KEY NECESSARY" }
+variable "secret_key" { default = "SECRET KEY NECESSARY" }
+variable "ssh_key"    { default = "SSH PUBLIC KEY NECESSARY" }
+
+# OTHERS
+variable "name"       { default = "msaez-" }
