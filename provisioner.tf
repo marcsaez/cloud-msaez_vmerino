87d7abdbcbecf7f093658bf254edfbd9762e791f
= <=>
2023-02-27 19:34:30 +0100

    def

--- provisioner.tf
+++ provisioner.tf
@@ -1,5 +1,5 @@
 # WEB 1
-resource "null_resource" "web1" {
+resource "null_resource" "file1" {
   triggers = {
     instance_id = aws_instance.hosting.id
   }
@@ -7,7 +7,7 @@ resource "null_resource" "web1" {
     type     = "ssh"
     user     = "ubuntu"
     host     = "${aws_instance.hosting.public_ip}"
-    private_key = "${file("/home/austria/.ssh/id_rsa")}"
+    private_key = "${file("~/.ssh/id_rsa")}"
   }
   provisioner "file" {
     source = "./webs/index.html"
@@ -15,7 +15,9 @@ resource "null_resource" "web1" {
   } 
   provisioner "remote-exec" {
     inline = [
-      "sudo mv /home/ubuntu/index1.html /var/www/web1/index.html"
+      "cat ~/index1.html",
+      "sleep 10",
+      "until sudo mv ~/index1.html /var/www/web1/index.html"
     ]
   }
   depends_on = [
@@ -24,7 +26,7 @@ resource "null_resource" "web1" {
   ]
 }
 
-resource "null_resource" "web2" {
+resource "null_resource" "web2s" {
   triggers = {
     instance_id = aws_instance.hosting.id
   }
@@ -32,15 +34,17 @@ resource "null_resource" "web2" {
     type     = "ssh"
     user     = "ubuntu"
     host     = "${aws_instance.hosting.public_ip}"
-    private_key = "${file("/home/austria/.ssh/id_rsa")}"
+    private_key = "${file("~/.ssh/id_rsa")}"
   }
   provisioner "file" {
     source = "./webs/DevBlog-Theme-master"
-    destination = "/home/ubuntu/."
+    destination = "/home/ubuntu/"
   } 
   provisioner "remote-exec" {
     inline = [
-      "sudo cp -r /home/ubuntu/DevBlog-Theme-master/* /var/www/web2/."
+      "ls -l ~/DevBlog-Theme-master",
+      "sleep 10",
+      "until sudo cp -r ~/DevBlog-Theme-master/* /var/www/web2/"
     ]
   }
   depends_on = [
