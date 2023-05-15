77ede4fa0a97590323009eb869e575a38e7c5591
= <=>
2023-05-15 19:07:17 +0200

    def

--- 03:tfstate/rancher.tf
+++ 03:tfstate/rancher.tf
@@ -34,104 +34,8 @@ resource "aws_security_group" "rancher_sg_allowall" {
   }
 
   ingress {
-    from_port   = "8080"
-    to_port     = "8080"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "6443"
-    to_port     = "6443"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "3260"
-    to_port     = "3260"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "30000"
-    to_port     = "30000"
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "30000"
-    to_port     = "30000"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "8472"
-    to_port     = "8472"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "2380"
-    to_port     = "2380"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "9099"
-    to_port     = "9099"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
     from_port   = "443"
     to_port     = "443"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "179"
-    to_port     = "179"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "-1"
-    to_port     = "-1"
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "2049"
-    to_port     = "2049"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "30000"
-    to_port     = "30000"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = ""
@@ -143,46 +47,6 @@ resource "aws_security_group" "rancher_sg_allowall" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = ""
-  }
-
-  ingress {
-    from_port   = "10250"
-    to_port     = "10250"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "2379"
-    to_port     = "2379"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "6443"
-    to_port     = "6443"
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "10254"
-    to_port     = "10254"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = ""
-  }
-
-  ingress {
-    from_port   = "8500"
-    to_port     = "8500"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Vault"
   }
 
   egress {
